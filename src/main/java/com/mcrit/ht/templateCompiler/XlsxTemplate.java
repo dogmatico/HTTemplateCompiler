53dea945c4824b0e2b47917d126a7a7362338b31
Cristian Lorenzo i Martínez <cristian.lorenzo.martinez@gmail.com>
2016-04-29 16:12:27 +0200

    Added styles support to Template Compiler.

--- src/main/java/com/mcrit/ht/templateCompiler/XlsxTemplate.java
+++ src/main/java/com/mcrit/ht/templateCompiler/XlsxTemplate.java
@@ -34,6 +34,8 @@ import java.io.StringReader;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.Iterator;
 
 import javax.json.Json;
 import javax.json.JsonArray;
@@ -61,6 +63,13 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.batik.transcoder.image.PNGTranscoder;
 import org.apache.batik.transcoder.TranscoderInput;
 import org.apache.batik.transcoder.TranscoderOutput;
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.FontUnderline;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFColor;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.extensions.XSSFCellBorder;
 
 /**
  * Class used in the HIGH-TOOL projecto to render XLSX templates.
@@ -102,8 +111,50 @@ public class XlsxTemplate {
         return sheet;
     }
     
-    private void populateTextSheet(JsonArray target, JsonArray data) {
+    private void applyStyleToSheet(JsonArray target, JsonArray data) {
         XSSFSheet sheet = getSheetByNameOrIndex(target);
+        
+        data.forEach(it -> {
+            JsonObject styleToApply = (JsonObject) it;
+            XSSFCellStyle style = stylesDict.get(styleToApply.getString("style"));
+            
+            if (style == null) {
+                throw new IllegalArgumentException("The style " + styleToApply.getString("style") + " does not exists. Unable to apply it to a sheet.");
+            }
+            
+            JsonArray targets = styleToApply.getJsonArray("targets");
+            
+            targets.forEach(targetCells -> {
+                int[] upperLeft = {
+                    ((JsonArray) targetCells).getJsonArray(0).getInt(0),
+                    ((JsonArray) targetCells).getJsonArray(0).getInt(1)
+                };
+                
+                int[] lowerRight = {
+                    ((JsonArray) targetCells).getJsonArray(1).getInt(0),
+                    ((JsonArray) targetCells).getJsonArray(1).getInt(1)
+                };
+                
+                for (int i = upperLeft[1]; i <= lowerRight[1]; i+= 1) {
+                    XSSFRow row = sheet.getRow(i); 
+                    if (row == null) {
+                        sheet.createRow(i);
+                        row = sheet.getRow(i);
+                    }
+                    for (int j = upperLeft[0]; j <= lowerRight[0]; j+= 1) {
+                        XSSFCell cell = row.getCell(j, XSSFRow.CREATE_NULL_AS_BLANK);
+                        cell.setCellStyle(style);
+                    }
+                }
+            });
+        });
+    }
+    
+    
+    private void populateTextSheet(JsonArray target, JsonArray data, String selectedStyle) {
+        XSSFSheet sheet = getSheetByNameOrIndex(target);
+        
+        XSSFCellStyle style = stylesDict.get(selectedStyle);
 
         for (int j = 0, ln2 = data.size(); j < ln2; j++) {
             XSSFRow row = sheet.getRow(j + target.getJsonArray(1).getInt(1));
@@ -122,10 +173,179 @@ public class XlsxTemplate {
                         cell.setCellValue(data.getJsonArray(j).getJsonNumber(k).doubleValue());
                         break;
                 }
+                
+                if (style != null) {
+                    cell.setCellStyle(style);
+                }
             }
         } 
     }
+    
+    private final HashMap<String, XSSFCellStyle> stylesDict = new HashMap();
+    
+    /**
+     * 
+     * @param JsonObject. The valid keys are:
+     * fontSize : int,
+     * fontColor : JsonArray[int, int, int] with rgb values,
+     * fontBold: boolean,
+     * fontItalic : boolean,
+     * fontUnderline : string, valid values DOUBLE, DOUBLE_ACCOUNTING, NONE, SINGLE, SINGLE_ACCOUNTING,
+     * backgroundColor : JsonArray[int, int, int] with rgb values,
+     * border : {"color": array of JsonArray[int, int, int] with rgb values as size, 
+     * "size" : int[] with [up, right, down, left] or [upDown, leftRight] or [all]}
+     * "style" : string[], as others. Valid values DASH_DOT, DASH_DOT_DOT, DASHED, DOTTED, DOUBLE,
+     * HAIR, MEDIUM, MEDIUM_DASH_DOT, MEDIUM_DASH_DOT_DOT, MEDIUM_DASHED, NONE, SLANTED_DASH_DOT, THICK, THIN;
+     * "align" : String, valid values: CENTER, CENTER_SELECTION, DISTRIBUTED, FILL,
+     * GENERAL, JUSTIFY, LEFT, RIGHT,
+     * 
+     */;
+    
+    /**
+     * 
+     * @param JsonObject. The valid keys are:
+     * fontSize : int,
+     * fontColor : JsonArray[int, int, int] with rgb values,
+     * fontBold: boolean,
+     * fontItalic : boolean,
+     * fontUnderline : string, valid values DOUBLE, DOUBLE_ACCOUNTING, NONE, SINGLE, SINGLE_ACCOUNTING,
+     * backgroundColor : JsonArray[int, int, int] with rgb values,
+     * border : {"color": array of JsonArray[int, int, int] with rgb values as size, 
+     * "size" : int[] with [up, right, down, left] or [upDown, leftRight] or [all]}
+     * "style" : string[], as others. Valid values DASH_DOT, DASH_DOT_DOT, DASHED, DOTTED, DOUBLE,
+     * HAIR, MEDIUM, MEDIUM_DASH_DOT, MEDIUM_DASH_DOT_DOT, MEDIUM_DASHED, NONE, SLANTED_DASH_DOT, THICK, THIN;
+     * "align" : String, valid values: CENTER, CENTER_SELECTION, DISTRIBUTED, FILL,
+     * GENERAL, JUSTIFY, LEFT, RIGHT,
+     * 
+     */
+    private void parseStylesObject(JsonObject styles) {
+        for (String styleName : styles.keySet()) {
+            JsonObject style = styles.getJsonObject(styleName);
+            XSSFCellStyle cellStyle = workbook.createCellStyle();
+            XSSFFont font = workbook.createFont();
+            
+            for (String styleProperty : style.keySet()) {
+                // Create color
+                XSSFColor color = null;
 
+                switch (styleProperty) {
+                    case "fontColor":
+                    case "backgroundColor":
+                        JsonArray rgb = style.getJsonArray(styleProperty);
+                        color = new XSSFColor(new java.awt.Color(
+                            rgb.getInt(0),
+                            rgb.getInt(1),
+                            rgb.getInt(2))
+                        );
+                        break;
+                }
+
+                switch (styleProperty) {
+                    case "fontSize":
+                        short ppt = (short) style.getInt(styleProperty);
+                        font.setFontHeightInPoints(ppt);
+                        break;
+                    case "fontColor":
+                        font.setColor(color);
+                        break;
+                    case "fontBold":
+                        boolean setBold = style.getBoolean(styleProperty);
+                        font.setBold(setBold);
+                        if (setBold) {
+                            font.setBoldweight(XSSFFont.BOLDWEIGHT_BOLD);
+                        } else {
+                           font.setBoldweight(XSSFFont.BOLDWEIGHT_NORMAL); 
+                        }
+
+                        break;
+                    case "fontItalic":
+                        font.setItalic(style.getBoolean(styleProperty));
+                        break;
+                    case "fontUnderline":
+                        font.setUnderline(FontUnderline.valueOf(style.getString(styleProperty)));
+                        break;
+                    case "backgroundColor":
+                        cellStyle.setFillBackgroundColor(color);
+                        cellStyle.setFillPattern(XSSFCellStyle.SOLID_FOREGROUND);
+                        break;
+                    case "border":
+                        JsonObject borderProperties = style.getJsonObject(styleProperty);
+                        for (String borderProperty : borderProperties.keySet()) {
+                            JsonArray borderStyleProp;
+                            borderStyleProp = borderProperties.getJsonArray(borderProperty);
+
+                            switch (borderProperty) {
+                                case "color":
+                                    XSSFColor[] colors = new XSSFColor[borderStyleProp.size()];
+
+                                    for (int i = 0; i < borderStyleProp.size(); i += 1) {
+                                        colors[i] = new XSSFColor(new java.awt.Color(
+                                                    borderStyleProp.getJsonArray(i).getInt(0),
+                                                    borderStyleProp.getJsonArray(i).getInt(1),
+                                                    borderStyleProp.getJsonArray(i).getInt(2))
+                                                );
+                                    }
+
+                                    if (colors != null) {
+                                        switch (colors.length) {
+                                            case 4:
+                                                cellStyle.setBorderColor(XSSFCellBorder.BorderSide.TOP, colors[0]);
+                                                cellStyle.setBorderColor(XSSFCellBorder.BorderSide.RIGHT, colors[1]);
+                                                cellStyle.setBorderColor(XSSFCellBorder.BorderSide.BOTTOM, colors[2]);
+                                                cellStyle.setBorderColor(XSSFCellBorder.BorderSide.LEFT, colors[3]);
+                                                break;
+                                            case 2:
+                                                cellStyle.setBorderColor(XSSFCellBorder.BorderSide.TOP, colors[0]);
+                                                cellStyle.setBorderColor(XSSFCellBorder.BorderSide.BOTTOM, colors[0]);
+                                                cellStyle.setBorderColor(XSSFCellBorder.BorderSide.RIGHT, colors[1]);
+                                                cellStyle.setBorderColor(XSSFCellBorder.BorderSide.LEFT, colors[1]);
+                                                break;
+                                            default:
+                                                cellStyle.setBorderColor(XSSFCellBorder.BorderSide.TOP, colors[0]);
+                                                cellStyle.setBorderColor(XSSFCellBorder.BorderSide.RIGHT, colors[0]);
+                                                cellStyle.setBorderColor(XSSFCellBorder.BorderSide.BOTTOM, colors[0]);
+                                                cellStyle.setBorderColor(XSSFCellBorder.BorderSide.LEFT, colors[0]);   
+                                        }
+                                    }
+                                    break;
+                                case "style" :
+                                    switch (borderStyleProp.size()) {
+                                        case 4:
+                                            cellStyle.setBorderTop(BorderStyle.valueOf(borderStyleProp.getString(0)));
+                                            cellStyle.setBorderRight(BorderStyle.valueOf(borderStyleProp.getString(1)));
+                                            cellStyle.setBorderBottom(BorderStyle.valueOf(borderStyleProp.getString(2)));
+                                            cellStyle.setBorderLeft(BorderStyle.valueOf(borderStyleProp.getString(3)));
+                                            break;
+                                        case 2:
+                                            cellStyle.setBorderTop(BorderStyle.valueOf(borderStyleProp.getString(0)));
+                                            cellStyle.setBorderBottom(BorderStyle.valueOf(borderStyleProp.getString(0)));
+                                            cellStyle.setBorderRight(BorderStyle.valueOf(borderStyleProp.getString(1)));
+                                            cellStyle.setBorderLeft(BorderStyle.valueOf(borderStyleProp.getString(1)));
+                                            break;
+                                        default:
+                                            cellStyle.setBorderTop(BorderStyle.valueOf(borderStyleProp.getString(0)));
+                                            cellStyle.setBorderRight(BorderStyle.valueOf(borderStyleProp.getString(1)));
+                                            cellStyle.setBorderBottom(BorderStyle.valueOf(borderStyleProp.getString(2)));
+                                            cellStyle.setBorderLeft(BorderStyle.valueOf(borderStyleProp.getString(3)));   
+                                    }
+                                    break;
+                                default :
+                                    throw new IllegalArgumentException("Unkown border property.");
+                            }
+                        }
+                        break;
+                    case "align":
+                        cellStyle.setAlignment(HorizontalAlignment.valueOf(style.getString(styleProperty)));
+                        break;
+                    default : 
+                        throw new IllegalArgumentException("Unkown property.");
+                }
+            }
+            
+            cellStyle.setFont(font);
+            stylesDict.put(styleName, cellStyle);
+        }
+    }
     /**
      * Adds and resize a image encoded in base64.
      * 
@@ -208,16 +428,23 @@ public class XlsxTemplate {
             JsonArray target = ((JsonObject) chunkToInsert).getJsonArray("target");
             JsonArray data = ((JsonObject) chunkToInsert).getJsonArray("data");
 
+            String selectedStyle = (((JsonObject) chunkToInsert).containsKey("style") ?
+                ((JsonObject) chunkToInsert).getString("style") : 
+                null);
+                    
             String chunkType;
             chunkType = (((JsonObject) chunkToInsert).containsKey("type") ?
                     ((JsonObject) chunkToInsert).getString("type") : "");
 
             switch (chunkType) {
+                case "styles":
+                    applyStyleToSheet(target, data);
+                    break;
                 case "imageBase64":
                     addImageBase64ToSheet(target, data);
                     break;
                 default:
-                    populateTextSheet(target, data);  
+                    populateTextSheet(target, data, selectedStyle);  
             }
         }
     }
@@ -262,7 +489,7 @@ public class XlsxTemplate {
     /**
      *
      * @param templatePath: URL of the template
-     * @param JSONString: A JSON string Array with the new data. The structure is:
+     * @param JsonArrayData: A JSON string Array with the new data. The structure is:
      *      [ { "type" : (String) Optional, if "imageBase64", will call a method to 
      *                   attach a image. Otherwise, will fill numbers
      *          "target" : 
@@ -272,14 +499,19 @@ public class XlsxTemplate {
      *              ],
      *          "data": [[]] ==> Matrix in standard notation rows, columns
      *       }]
+     * @param JsonObjectStyles : A JSON string object. Each key is a style. The keys of 
+     * the style define properties.
      * @throws IOException String templatePath, String JSONString
      * @throws org.apache.batik.transcoder.TranscoderException
      */
-    static public void compileAndStreamTemplate(String templatePath, String JSONString) throws IOException, TranscoderException {
-        JsonReader jsonReader = Json.createReader(new StringReader(JSONString));
-
+    static public void compileAndStreamTemplate(String templatePath, String JsonArrayData, String JsonObjectStyles) throws IOException, TranscoderException {
+        JsonArray data = Json.createReader(new StringReader(JsonArrayData)).readArray();
+        JsonObject styles = Json.createReader(new StringReader(JsonObjectStyles)).readObject();
+        
+        
         XlsxTemplate instance = new XlsxTemplate(templatePath);
-        instance.compileTemplate(jsonReader.readArray());
+        instance.parseStylesObject(styles);
+        instance.compileTemplate(data);
         instance.recalculateAll();
         instance.streamWorkbook(System.out);
     }
